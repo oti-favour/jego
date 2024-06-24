6ae3efb01e82344e30fc9c9503f8c6dd18d67e16
Zerebel <86872654+Zerebel@users.noreply.github.com>
2024-06-24 22:09:05 +0100

    refactor: Remove unused code and update page transition in Template component

--- app/template.tsx
+++ app/template.tsx
@@ -1,27 +1,10 @@
-"use client";
-import { useEffect } from "react";
-
-import { animatePageIn } from "@/animations";
+import Transition from "@/components/Transition";
 
 function Template({ children }: { children: React.ReactNode }) {
-  useEffect(() => {
-    animatePageIn();
-  }, []);
-
   return (
     <div>
       {children}
-      <div
-        id="loading"
-        className="flex h-svh w-svw fixed top-0 left-0"
-        suppressHydrationWarning
-      >
-        <div className="bg-loading bg-fixed w-full bg-cover"></div>
-        <div className="bg-loading bg-fixed w-full h-full bg-cover"></div>
-        <div className="bg-loading bg-fixed w-full h-full bg-cover"></div>
-        <div className="bg-loading bg-fixed w-full h-full bg-cover"></div>
-        <div className="bg-loading bg-fixed bg-cover w-full h-full"></div>
-      </div>
+      <Transition />
     </div>
   );
 }
