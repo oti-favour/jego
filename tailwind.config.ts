a756130a1f62efb800bc0e77b17452d2702bf48f
Zerebel <86872654+Zerebel@users.noreply.github.com>
2024-06-24 19:59:47 +0100

    feat: Add new background images and sizes to tailwind theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,17 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        loading: "url('/assets/loadingBg.svg')",
+        home_ray: "url('/assets/rays-left.png')",
+        home_car: "url('/assets/home_car.png')",
+      },
+      backgroundSize: {
+        "80": "80%",
+        "150": "150%",
+      },
+    },
   },
   plugins: [],
 };
