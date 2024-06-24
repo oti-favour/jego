3312f3ee298ccc1b7cd963dfe89bf65f5ed4370e
Zerebel <86872654+Zerebel@users.noreply.github.com>
2024-06-24 22:09:10 +0100

    refactor: Update animation duration and delay in animatePageIn function

--- animations/animation.ts
+++ animations/animation.ts
@@ -7,25 +7,17 @@ export function animatePageIn() {
   if (!ref) return console.log("no loading");
   const tl = gsap.timeline();
 
-  //   animate the 5 divs in the loading screen with different delays by looping through them
   tl.to(ref.children, {
-    duration: 1,
+    duration: 2,
     yPercent: -100,
     backgroundAttachment: "fixed",
+    delay: 0.5,
     stagger: {
       from: "random",
       amount: 0.5,
     },
     ease: "power4.inOut",
   });
-  //   .to(ref, {
-  //     duration: 0.7,
-  //     yPercent: -100,
-  //     ease: "power4.inOut",
-  //     onComplete: () => {
-  //       ref.style.display = "none";
-  //     },
-  //   });
 }
 
 export function animatePageOut(href: string, router: AppRouterInstance) {
