b63dd986e691bc0f93aa5d488b26aa86978555d9
Zerebel <86872654+Zerebel@users.noreply.github.com>
2024-06-25 10:42:53 +0100

    feat: Add Logo component for displaying the logo image

---
+++ components/Logo.tsx
@@ -0,0 +1,43 @@
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import React from "react";
+
+const Logo: React.FC<LogoProps> = ({ ...props }) => {
+  const sizes = () => {
+    switch (props.size) {
+      case "sm":
+        return 500;
+      case "md":
+        return 700;
+      case "lg":
+        return 900;
+      default:
+        return 500;
+    }
+  };
+
+  const size = sizes();
+
+  return (
+    <Image
+      src={
+        props.variant === "secondary"
+          ? "/assets/logo-black.svg"
+          : "/assets/logo.svg"
+      }
+      alt="logo"
+      width={size}
+      height={size}
+      className={cn("w-auto h-auto", props.className)}
+      {...props}
+    />
+  );
+};
+
+interface LogoProps {
+  className?: string;
+  variant?: "primary" | "secondary";
+  size?: "sm" | "md" | "lg";
+}
+
+export default Logo;
