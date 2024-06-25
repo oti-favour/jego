ee8e584d68897ef909b0f5ede10c25c1167d9973
Zerebel <86872654+Zerebel@users.noreply.github.com>
2024-06-25 10:43:09 +0100

    refactor: Update HomeLayout to include padding for better layout consistency

--- components/home/HomeLayout.tsx
+++ components/home/HomeLayout.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function HomeLayout({ children }: { children?: React.ReactNode }) {
   return (
     <div className="bg-home_car bg-center bg-cover bg-no-repeat h-svh">
-      <div className="bg-home_ray bg-150 md:bg-80 bg-no-repeat h-full">
+      <div className="bg-home_ray bg-150 md:bg-80 bg-no-repeat h-full px-4">
         {children}
       </div>
     </div>
