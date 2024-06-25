4d422daf3494d204add4169e060438ec42a66153
Zerebel <86872654+Zerebel@users.noreply.github.com>
2024-06-25 10:43:15 +0100

    feat: Add z-index to Transition component for better layering

--- components/Transition.tsx
+++ components/Transition.tsx
@@ -11,7 +11,7 @@ function Transition() {
   return (
     <div
       id="loading"
-      className="flex h-svh w-svw fixed top-0 left-0"
+      className="flex h-svh w-svw fixed top-0 left-0 z-50"
       suppressHydrationWarning
     >
       <div className="bg-loading bg-fixed w-full bg-cover"></div>
