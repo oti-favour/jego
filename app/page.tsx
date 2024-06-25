d9fce7270a1ce76f5968211a1e64d7e1b1875bae
Zerebel <86872654+Zerebel@users.noreply.github.com>
2024-06-25 10:42:48 +0100

    feat: Add Navbar component to HomeLayout

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
+import Navbar from "@/components/Navbar";
 import HomeLayout from "@/components/home/HomeLayout";
-import Image from "next/image";
 
 export default async function Home() {
-  return <HomeLayout></HomeLayout>;
+  return (
+    <HomeLayout>
+      <Navbar />
+    </HomeLayout>
+  );
 }
