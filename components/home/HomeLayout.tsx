3e8c7739bcac63eff7c6abff1bf142517654737b
Zerebel <86872654+Zerebel@users.noreply.github.com>
2024-06-26 15:57:16 +0100

    chore: Refactor HomeLayout component for improved readability and maintainability

--- components/home/HomeLayout.tsx
+++ components/home/HomeLayout.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function HomeLayout({ children }: { children?: React.ReactNode }) {
   return (
-    <div className="bg-home_car bg-center bg-cover bg-no-repeat h-svh">
-      <div className="bg-home_ray bg-150 md:bg-80 bg-no-repeat h-full px-4">
+    <div className="min-h-svh bg-home_car bg-cover bg-center bg-no-repeat">
+      <div className="h-full bg-home_ray bg-150 bg-no-repeat px-4 md:bg-80">
         {children}
       </div>
     </div>
