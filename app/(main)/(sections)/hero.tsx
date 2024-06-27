c600e26a285ff0743c67262fa9dc47386154f8e6
Zerebel <86872654+Zerebel@users.noreply.github.com>
2024-06-27 09:53:37 +0100

    refactor: Update HeroSection component to use HeroLayout and add HeadingOne and HeadingThree components

--- app/(main)/(sections)/hero.tsx
+++ app/(main)/(sections)/hero.tsx
@@ -1,20 +1,22 @@
-import CTA from "@/components/CTA";
+import CTA, { CTASecondary } from "@/components/CTA";
 import FeaturesStepper from "@/components/FeaturesStepper";
 import Navbar from "@/components/Navbar";
-import { Button } from "@/components/ui/button";
+import HeadingOne, { HeadingThree } from "@/components/Typography";
+import HeroLayout from "@/components/home/HeroLayout";
 import ScrollIcon from "@/public/assets/scroll-icon.svg";
 import { Hero, Overview } from "@/types/generated-types";
 
 async function HeroSection({ hero }: { hero: Hero }) {
   return (
-    <div className="flex h-full min-h-svh flex-col py-12 lg:container">
-      <Navbar />
-
-      <div className="flex h-full flex-1 flex-col justify-center space-y-24">
-        <HeroOverview overview={hero.overview} />
-        <FeaturesStepper features={hero.pageFeatures} />
+    <HeroLayout>
+      <div className="flex h-full min-h-svh flex-col pt-12 lg:container">
+        <Navbar />
+        <div className="flex h-full flex-1 flex-col justify-center space-y-24">
+          <HeroOverview overview={hero.overview} />
+          <FeaturesStepper features={hero.pageFeatures} />
+        </div>
       </div>
-    </div>
+    </HeroLayout>
   );
 }
 
@@ -22,23 +24,14 @@ function HeroOverview({ overview }: { overview: Overview }) {
   return (
     <div className="mt-16 space-y-16 text-white">
       <div className="flex justify-end">
-        <h3 className="max-w-xs text-sm md:max-w-sm md:text-lg">
-          {overview.description}
-        </h3>
+        <HeadingThree text={overview.description} />
       </div>
       <div className="max-w-5xl space-y-8">
-        <h1
-          className={`font-gustavo text-4xl font-bold uppercase leading-[64px] md:text-8xl`}
-        >
-          {overview.title}
-        </h1>
-
+        <HeadingOne text={overview.title} />
         <div className="flex justify-between">
           <div className="grid max-w-fit flex-1 grid-rows-2 gap-4 lg:grid-cols-2 lg:grid-rows-1">
             <CTA href="" text={overview.ctaText || ""} />
-            <Button className="h-full max-w-fit rounded-full bg-white/10 px-12 backdrop-blur-sm">
-              Learn More
-            </Button>
+            <CTASecondary text="Learn More" />
           </div>
           <ScrollIcon className="animate-bounce self-end" />
         </div>
