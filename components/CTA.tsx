d33bd7724fadb1bd3b82476f2595afce522ea3dc
Zerebel <86872654+Zerebel@users.noreply.github.com>
2024-06-27 09:53:50 +0100

    refactor: Update CTA component to include CTAPlain and CTASecondary

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -1,18 +1,19 @@
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import React from "react";
+import { Button } from "./ui/button";
 
 const CTA: React.FC<CTAProps & React.HTMLProps<HTMLAnchorElement>> = (
-  props
+  props,
 ) => {
   return (
     <Link href={props.href} className="">
       {props.children ? (
         props.children
       ) : (
-        <div className="flex items-center bg-white max-w-fit text-black py-2 px-2 rounded-full gap-4">
+        <div className="flex max-w-fit items-center gap-4 rounded-full bg-white px-2 py-2 text-black">
           <span className={cn("ml-4", "font-bold")}>{props.text}</span>
-          <span className="w-10 h-10 flex justify-center items-center rounded-full bg-darkOrange">
+          <span className="flex h-10 w-10 items-center justify-center rounded-full bg-darkOrange">
             {props.icon ? (
               props.icon
             ) : (
@@ -27,11 +28,46 @@ const CTA: React.FC<CTAProps & React.HTMLProps<HTMLAnchorElement>> = (
   );
 };
 
+const CTAPlain: React.FC<CTAProps & React.HTMLProps<HTMLAnchorElement>> = (
+  props,
+) => {
+  return (
+    <Link
+      href={props.href}
+      className={cn(
+        "flex max-w-fit items-center gap-4 rounded-full bg-white px-2 py-2 text-black",
+        props.className,
+      )}
+    >
+      {props.children ? props.children : <>{props.text}</>}
+    </Link>
+  );
+};
+
 interface CTAProps {
   href: string;
   text: string;
   children?: React.ReactNode;
   icon?: React.ReactNode;
+  className?: string;
+}
+
+function CTASecondary({
+  text,
+  onClick,
+}: {
+  text: string;
+  onClick?: () => void;
+}) {
+  return (
+    <Button
+      className="h-full max-w-fit rounded-full bg-white/10 px-12 backdrop-blur-sm"
+      onClick={onClick}
+    >
+      {text}
+    </Button>
+  );
 }
 
 export default CTA;
+export { CTAPlain, CTASecondary };
