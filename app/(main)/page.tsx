aa49cf9a9348fd560005324d6ab3b2357b7bffa2
Zerebel <86872654+Zerebel@users.noreply.github.com>
2024-06-27 09:53:42 +0100

    refactor: Update Main component to use HeroSection and IntroSection

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,10 +1,10 @@
-import HeroLayout from "@/components/home/HeroLayout";
 import { Home } from "@/types/generated-types";
 import { fetchDocument } from "@/utils/actions/actions";
 import { GetHomePage } from "@/utils/queries/queries";
 import { notFound } from "next/navigation";
 import { cache } from "react";
 import HeroSection from "./(sections)/hero";
+import IntroSection from "./(sections)/intro";
 
 const getData = cache(async () => {
   const data = await fetchDocument<Home>({
@@ -21,15 +21,13 @@ async function Main() {
     return notFound();
   }
 
-  const { hero } = data;
+  const { hero, intro } = data;
 
   return (
-    <div>
-      <HeroLayout>
-        <HeroSection hero={hero} />
-      </HeroLayout>
-      <div className="h-96">adlk</div>
-    </div>
+    <>
+      <HeroSection hero={hero} />
+      <IntroSection intro={intro} />
+    </>
   );
 }
 
