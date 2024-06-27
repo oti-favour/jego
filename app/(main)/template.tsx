2297bac05568e47735c369ea29475205c173c74b
Zerebel <86872654+Zerebel@users.noreply.github.com>
2024-06-27 12:30:35 +0100

    refactor: Optimize Transition component and improve loading behavior

--- app/(main)/template.tsx
+++ app/(main)/template.tsx
@@ -4,8 +4,7 @@ import HomeDataProvider from "@/hooks/useHomeData";
 function Template({ children }: { children: React.ReactNode }) {
   return (
     <HomeDataProvider>
-      {children}
-      <Transition />
+      <Transition>{children}</Transition>
     </HomeDataProvider>
   );
 }
--- components/Transition.tsx
+++ components/Transition.tsx
@@ -4,7 +4,7 @@ import { animatePageIn } from "@/animations";
 import { HomePageContext } from "@/hooks/useHomeData";
 import { useContext, useEffect } from "react";
 
-function Transition() {
+function Transition({ children }: { children: React.ReactNode }) {
   const { data, isLoading } = useContext(HomePageContext);
 
   useEffect(() => {
@@ -14,17 +14,20 @@ function Transition() {
   }, [data, isLoading]);
 
   return (
-    <div
-      id="loading"
-      className="fixed left-0 top-0 z-50 flex h-svh w-svw"
-      suppressHydrationWarning
-    >
-      <div className="w-full bg-loading bg-cover bg-fixed"></div>
-      <div className="h-full w-full bg-loading bg-cover bg-fixed"></div>
-      <div className="h-full w-full bg-loading bg-cover bg-fixed"></div>
-      <div className="h-full w-full bg-loading bg-cover bg-fixed"></div>
-      <div className="h-full w-full bg-loading bg-cover bg-fixed"></div>
-    </div>
+    <>
+      {!isLoading && children}
+      <div
+        id="loading"
+        className="fixed left-0 top-0 z-50 flex h-svh w-svw"
+        suppressHydrationWarning
+      >
+        <div className="w-full bg-loading bg-cover bg-fixed"></div>
+        <div className="h-full w-full bg-loading bg-cover bg-fixed"></div>
+        <div className="h-full w-full bg-loading bg-cover bg-fixed"></div>
+        <div className="h-full w-full bg-loading bg-cover bg-fixed"></div>
+        <div className="h-full w-full bg-loading bg-cover bg-fixed"></div>
+      </div>
+    </>
   );
 }
 
