f0df95b4707d9cce2525ff91e324c32181b72b20
Zerebel <86872654+Zerebel@users.noreply.github.com>
2024-06-27 09:53:55 +0100

    refactor: Update Navbar component to include lg:container class

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -24,7 +24,7 @@ import {
 
 function Navbar() {
   return (
-    <header>
+    <header className="lg:container">
       <MobileNav components={NavbarConst} />
       <DesktopNav components={NavbarConst} />
     </header>
