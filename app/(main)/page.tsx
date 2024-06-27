c512e833a2c0e4440066d259daccca1cfe456118
Zerebel <86872654+Zerebel@users.noreply.github.com>
2024-06-27 12:13:18 +0100

    refactor: Update Main component to use useContext for fetching home page data

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,29 +1,17 @@
-import { Home } from "@/types/generated-types";
-import { fetchDocument } from "@/utils/actions/actions";
-import { GetHomePage } from "@/utils/queries/queries";
+"use client";
+import { HomePageContext } from "@/hooks/useHomeData";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import { notFound } from "next/navigation";
-import { cache } from "react";
+import { useContext } from "react";
 import HeroSection from "./(sections)/hero";
 import IntroSection from "./(sections)/intro";
 
-const getData = cache(async () => {
-  const data = await fetchDocument<Home>({
-    query: GetHomePage,
-  });
-
-  return data;
-});
-
-async function Main() {
-  const data = await getData();
-
+function Main() {
+  const { data } = useContext(HomePageContext);
   if (!data) {
-    return notFound();
+    return null;
   }
 
   const { hero, intro } = data;
-
   return (
     <>
       <HeroSection hero={hero} />
