f170a5eecb714cd0bf22c2e316bf0a9f56c94761
Zerebel <86872654+Zerebel@users.noreply.github.com>
2024-06-27 21:32:14 +0100

    refactor: Update Main component to use getHomeData hook for fetching data

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,14 +1,14 @@
-"use client";
-import { HomePageContext } from "@/hooks/useHomeData";
+import getData from "@/hooks/getHomeData";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import { useContext } from "react";
+import { notFound } from "next/navigation";
 import HeroSection from "./(sections)/hero";
 import IntroSection from "./(sections)/intro";
 
-function Main() {
-  const { data } = useContext(HomePageContext);
+async function Main() {
+  const data = await getData();
+
   if (!data) {
-    return null;
+    return notFound();
   }
 
   const { hero, intro, powerPod } = data;
