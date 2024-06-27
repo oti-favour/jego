a740a1bd9f07072d3aa4828fbb70b73ddb938b7c
Zerebel <86872654+Zerebel@users.noreply.github.com>
2024-06-27 09:54:10 +0100

    refactor: Update types.ts to export Heading type

--- types/types.ts
+++ types/types.ts
@@ -1,7 +1,6 @@
-interface NavItemProps {
-  href: string;
-  text?: string;
-  icon?: React.ReactNode;
-  rounded?: boolean;
-  roundedClass?: string;
-}
+export type Heading = React.FC<
+  {
+    text: string;
+    className?: string;
+  } & React.HTMLAttributes<HTMLHeadingElement>
+>;
