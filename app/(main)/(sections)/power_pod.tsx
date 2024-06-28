33419558de8534eb5e4b9d670c3a55c85e80a7e8
Zerebel <86872654+Zerebel@users.noreply.github.com>
2024-06-28 07:04:01 +0100

    refactor: Update PowerPodSection component to improve layout structure and add WatchVideo component

--- app/(main)/(sections)/power_pod.tsx
+++ app/(main)/(sections)/power_pod.tsx
@@ -13,32 +13,37 @@ function PowerPodSection({ powerPod }: { powerPod: PowerPod }) {
   const restOfTitle = overview.title.split(" ").slice(1).join(" ");
   return (
     <PowerPodLayout>
-      <div className="flex items-center">
-        <div className="space-y-8 py-12 text-white lg:container">
-          <PageName text={overview.pageName} />
-          <div className="flex flex-col-reverse gap-8 md:flex-col">
-            <div className="max-w-4xl space-y-8">
-              <HeadingThree text={firstWord} className="space-x-4 text-white">
-                <span className="relative text-brightTurquoise">
-                  <span className="absolute -bottom-3 -left-8 h-full w-full bg-lightning bg-cover bg-left-bottom bg-no-repeat bg-blend-difference md:-bottom-4 md:-left-24 md:bg-auto"></span>
-                  {firstWord}
-                </span>
-                <span>{restOfTitle}</span>
-              </HeadingThree>
-              <Paragraph text={overview.description} />
-              <div>
-                <CTA text={overview.ctaText || ""} href={overview.cta || ""} />
+      <div className="flex h-full w-full flex-col px-4 lg:container">
+        <PageName text={overview.pageName} />
+        <div className="flex h-full flex-col justify-center">
+          <div className="space-y-8 py-12 text-white">
+            <div className="flex flex-col-reverse gap-8 md:flex-col">
+              <div className="max-w-4xl space-y-8">
+                <HeadingThree text={firstWord} className="space-x-4 text-white">
+                  <span className="relative text-brightTurquoise">
+                    <span className="absolute -bottom-3 -left-8 h-full w-full bg-lightning bg-cover bg-left-bottom bg-no-repeat bg-blend-difference md:-bottom-4 md:-left-24 md:bg-auto"></span>
+                    {firstWord}
+                  </span>
+                  <span>{restOfTitle}</span>
+                </HeadingThree>
+                <Paragraph text={overview.description} />
+                <div>
+                  <CTA
+                    text={overview.ctaText || ""}
+                    href={overview.cta || ""}
+                  />
+                </div>
+              </div>
+              <div className="flex justify-end py-8 md:justify-center md:py-12 md:pl-[40%] 2xl:py-24">
+                <WatchVideo videoUrl={video.videoUrl} title={video.title} />
               </div>
             </div>
-            <div className="flex justify-end md:justify-center md:pl-[40%]">
-              <WatchVideo videoUrl={video.videoUrl} title={video.title} />
+            <div>
+              <div className="lg:max-w-[40svw]">
+                <FeaturesStepper features={pageFeatures} />
+              </div>
+              {/* TODO: Scrol Icon*/}
             </div>
-          </div>
-          <div>
-            <div className="lg:max-w-[40svw]">
-              <FeaturesStepper features={pageFeatures} />
-            </div>
-            {/* TODO: Scrol Icon*/}
           </div>
         </div>
       </div>
