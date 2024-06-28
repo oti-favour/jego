c3719e747a57ecefb6a187d13df78481548b48e0
Zerebel <86872654+Zerebel@users.noreply.github.com>
2024-06-28 07:03:50 +0100

    refactor: Update PowerPodLayout component to improve layout structure

--- components/home/PowerPodLayout.tsx
+++ components/home/PowerPodLayout.tsx
@@ -1,7 +1,7 @@
 function PowerPodLayout({ children }: { children?: React.ReactNode }) {
   return (
     <div className="min-h-full bg-power_pod bg-cover bg-center bg-no-repeat">
-      <div className="h-full min-h-svh w-full bg-gradient-to-r from-black via-black/50 to-black/50 px-4">
+      <div className="h-full min-h-svh w-full bg-gradient-to-r from-black via-black/50 to-black/50">
         {children}
       </div>
     </div>
