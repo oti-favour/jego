cf2baed5502d5f64735362f7b67ba14830f3b099
Zerebel <86872654+Zerebel@users.noreply.github.com>
2024-07-07 10:49:39 +0100

    feat: Add MembersCarousel component for displaying member information

--- app/(main)/(sections)/about_us.tsx
+++ app/(main)/(sections)/about_us.tsx
@@ -1,4 +1,5 @@
 import { CTAPlain } from "@/components/CTA";
+import MembersCarousel from "@/components/Member";
 import PageName from "@/components/PageName";
 import { Caption, DynamicHeading, Paragraph } from "@/components/Typography";
 import { sanityImageUrl } from "@/lib/utils";
@@ -7,28 +8,30 @@ import { SanityAsset } from "@sanity/image-url/lib/types/types";
 import Image from "next/image";
 
 function AboutUsSection({ about }: { about: About }) {
-  const { overview, image, Members } = about;
+  const { overview, image, members } = about;
 
   return (
     <div className="-z-10 h-full min-h-svh w-full">
-      <div className="space-y-8 px-4 lg:container">
+      <div className="space-y-8 px-4 lg:container lg:space-y-16">
         <PageName text={overview.pageName} className="text-[#64748B]" />
         <div className="space-y-8 pb-12">
           <AboutUsOverview overview={overview} image={image} />
         </div>
       </div>
-      <div className="pointer-events-none flex overflow-x-hidden lg:-mt-32 2xl:-mt-40">
-        <div className="animate-marquee inline-block h-fit w-full whitespace-nowrap">
-          <div className="font-dela -z-50 h-fit text-[167px] text-[#E8E8E8] lg:text-[267px]">
+      <div className="pointer-events-none -mt-12 flex overflow-x-hidden lg:-mt-32 2xl:-mt-40">
+        <div className="inline-block h-fit w-full animate-marquee whitespace-nowrap">
+          <div className="-z-50 h-fit font-dela text-[167px] text-[#E8E8E8] lg:text-[267px]">
             <div className="-rotate-[4.975deg]">JeGO . JeGO</div>
           </div>
         </div>
       </div>
-      <div className="space-y-8 px-4 lg:container">
+      <div className="select-none space-y-8 px-4 lg:container">
         <Caption
           className="font-gustavo font-bold lg:text-5xl"
           text={"the corporate team"}
         />
+
+        <MembersCarousel members={members} />
       </div>
     </div>
   );
@@ -62,14 +65,13 @@ function AboutUsOverview({
       <div></div>
       {image && (
         <div className="relative z-50">
-          <div className="relative mx-auto max-w-xs md:max-w-[90%] lg:mx-0 lg:max-w-[90%] 2xl:max-w-full">
+          <div className="relative mx-auto max-w-[90%] md:max-w-[90%] lg:mx-0 lg:max-w-[90%] 2xl:max-w-full">
             <Image
               src={sanityImageUrl(image)}
               alt={image.asset._ref}
               width={1920}
               height={1080}
-              layout="responsive"
-              className="w-fit lg:w-auto"
+              className="h-full w-full"
             />
             <div className="absolute left-0 top-0 -z-50 h-full w-full rotate-[8.2deg] bg-[#FFF5EB]"></div>
           </div>
