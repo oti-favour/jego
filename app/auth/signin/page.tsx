78523bedee5e5fd0b3bc957d3e0c0c0a15cb0462
Zerebel <86872654+Zerebel@users.noreply.github.com>
2024-07-12 15:01:53 +0100

    feat: Add SignIn page component
    This commit adds the SignIn page component, which is responsible for rendering the sign-in form and other related elements. The component includes the necessary imports and JSX code to create the sign-in page layout. This addition enhances the application by providing a dedicated page for users to sign in and access their accounts.

---
+++ app/auth/signin/page.tsx
@@ -0,0 +1,41 @@
+import FormEnquiryButton from "@/components/FormEnquiryButton";
+import GoBack from "@/components/GoBack";
+import SignInForm from "@/components/SignInForm";
+import { Caption, DynamicHeading } from "@/components/Typography";
+
+function SignIn() {
+  return (
+    <div className="min-h-svh w-full bg-black">
+      <div className="flex h-full min-h-svh w-full flex-col bg-ray bg-cover bg-right bg-no-repeat">
+        <div className="flex h-full min-h-svh w-full flex-col lg:container">
+          <GoBack href="/" className="mt-8 text-white lg:mt-12" />
+          <div className="grid h-full w-full flex-1 place-items-center text-white">
+            <div className="flex h-full flex-col items-center justify-around">
+              <div className="flex h-full flex-col items-center justify-center">
+                <Caption>welcome to</Caption>
+                <DynamicHeading
+                  level="h1"
+                  className="max-w-screen-sm text-center text-white lg:max-w-screen-lg"
+                >
+                  JéGO&apos;S{" "}
+                  <span className="uppercase text-brightTurquoise">
+                    Investors{" "}
+                  </span>
+                  portal
+                </DynamicHeading>
+                <div className="mt-12">
+                  <SignInForm />
+                </div>
+              </div>
+              <div className="basis-1/4">
+                <FormEnquiryButton />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SignIn;
