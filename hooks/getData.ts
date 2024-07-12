5c54c3ab819cd364997bc3fd2e6f7ac1c0b8d76c
Zerebel <86872654+Zerebel@users.noreply.github.com>
2024-07-12 14:59:56 +0100

    feat: Add hooks for fetching home, investors, news, and reports data
    This commit adds hooks for fetching home, investors, news, and reports data. The `getHomeData` hook fetches the data for the home page, including the main content and footer. The `getInvestorsData` hook fetches the data for the investors page, including the main content and footer. The `getNewsDataFromRef` hook fetches the news data based on the provided news IDs. The `getOneNewsDataFromRef` hook fetches a single news item based on the provided slug. The `getReports` hook fetches all the reports data. The `getReportFromSlug` hook fetches a single report based on the provided slug. These hooks enhance the data fetching capabilities and provide the necessary data for rendering the respective pages and components.

---
+++ hooks/getData.ts
@@ -0,0 +1,91 @@
+import {
+  Footer,
+  Home,
+  Investors,
+  News,
+  Reports,
+} from "@/types/generated-types";
+import { fetchDocument, fetchDocumentByRef } from "@/utils/actions/actions";
+import {
+  GetFooter,
+  GetHomePage,
+  GetInvestorsPage,
+  GetNewsFromRef,
+  GetNewsFromSlug,
+  GetReportBySlug,
+  GetReports,
+} from "@/utils/queries/queries";
+import { cache } from "react";
+
+export const getHomeData = cache(async () => {
+  "use server";
+  const data = await fetchDocument<Home>({
+    query: GetHomePage,
+  });
+
+  const footer = await fetchDocument<Footer>({
+    query: GetFooter,
+  });
+
+  return { data, footer };
+});
+
+export default getHomeData;
+
+export const getInvestorsData = cache(async () => {
+  "use server";
+  const data = await fetchDocument<Investors>({
+    query: GetInvestorsPage,
+  });
+
+  const footer = await fetchDocument<Footer>({
+    query: GetFooter,
+  });
+
+  return { data, footer };
+});
+
+export const getNewsDataFromRef = cache(async (newsIds: string[]) => {
+  "use server";
+  const news = [];
+  for (const id of newsIds) {
+    const newsItem = await fetchDocumentByRef<News>({
+      query: GetNewsFromRef,
+      ref: id,
+    });
+    if (newsItem) {
+      news.push(newsItem);
+    }
+  }
+
+  return news;
+});
+
+export const getOneNewsDataFromRef = cache(async (slug: string) => {
+  "use server";
+  const newsItem = await fetchDocumentByRef<News>({
+    query: GetNewsFromSlug,
+    ref: slug,
+  });
+
+  return newsItem;
+});
+
+export const getReports = cache(async () => {
+  "use server";
+  const reports = await fetchDocument<Reports[]>({
+    query: GetReports,
+  });
+
+  return reports;
+});
+
+export const getReportFromSlug = cache(async (slug: string) => {
+  "use server";
+  const report = await fetchDocumentByRef<Reports>({
+    query: GetReportBySlug,
+    ref: slug,
+  });
+
+  return report;
+});
