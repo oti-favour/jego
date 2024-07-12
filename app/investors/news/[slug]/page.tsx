61dceeffbdfd83753263396f343aa414cdafed28
Zerebel <86872654+Zerebel@users.noreply.github.com>
2024-07-12 15:01:14 +0100

    feat: Add NewsDetails component for displaying detailed news content
    This commit adds the NewsDetails component, which is responsible for displaying detailed news content. The component fetches the news data based on the provided slug and renders the news header, title, author information, and content. It enhances the user experience by providing a dedicated page for viewing and reading news articles.

---
+++ app/investors/news/[slug]/page.tsx
@@ -0,0 +1,52 @@
+import BlockContent from "@/components/BlockContent";
+import FooterSection from "@/components/Footer";
+import GoBack from "@/components/GoBack";
+import Navbar from "@/components/Navbar";
+import { AuthorItem, NewsHeader } from "@/components/NewsItem";
+import { DynamicHeading } from "@/components/Typography";
+import { getInvestorsData, getOneNewsDataFromRef } from "@/hooks/getData";
+import { notFound } from "next/navigation";
+
+async function NewsDetails({ params: { slug } }: { params: { slug: string } }) {
+  const news = await getOneNewsDataFromRef(slug);
+  const { footer } = await getInvestorsData();
+
+  if (!news || !footer) {
+    return notFound();
+  }
+  return (
+    <div className="space-y-12">
+      <div className="container mt-2 space-y-8 lg:mt-8">
+        <Navbar desktopClassName="text-black" />
+        <div className="mx-auto w-fit space-y-8">
+          <GoBack href="/investors" />
+          <div className="space-y-2">
+            <NewsHeader {...news} />
+            {/* <div>
+              <Image
+                src={sanityImageUrl(news.image)}
+                alt={news.title}
+                width={1920}
+                height={1080}
+                className="aspect-video h-full w-full object-cover"
+              />
+            </div> */}
+            <DynamicHeading
+              level="h1"
+              className="!font-product text-2xl font-bold md:text-4xl lg:max-w-screen-md"
+            >
+              {news.title}
+            </DynamicHeading>
+          </div>
+          <AuthorItem {...news.author} />
+          <div className="whitespace-pre-line lg:max-w-screen-md">
+            <BlockContent value={news.content} />
+          </div>
+        </div>
+      </div>
+      <FooterSection footer={footer} />
+    </div>
+  );
+}
+
+export default NewsDetails;
