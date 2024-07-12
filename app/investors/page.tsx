830cff37b3ea277b00d880f3bb286fe8ac461fa3
Zerebel <86872654+Zerebel@users.noreply.github.com>
2024-07-12 15:01:40 +0100

    feat: Add lazy loading for images on investors page
    This commit adds lazy loading functionality for images on the investors page. By implementing lazy loading, the page load time is improved as images are loaded only when they are visible in the viewport. This enhancement enhances the overall performance and user experience of the application.

--- app/investors/page.tsx
+++ app/investors/page.tsx
@@ -1,5 +1,28 @@
-function Page() {
-  return <div>Investors</div>;
+import FooterSection from "@/components/Footer";
+import { getInvestorsData } from "@/hooks/getData";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { notFound } from "next/navigation";
+import InvestorsHeroSection from "./(sections)/hero";
+import NewsSection from "./(sections)/news";
+import ReportsSection from "./(sections)/reports";
+
+async function Page() {
+  const { data, footer } = await getInvestorsData();
+
+  if (!data || !footer) {
+    return notFound();
+  }
+  return (
+    <>
+      <div className="space-y-6 md:space-y-10 lg:space-y-12">
+        <InvestorsHeroSection home={data.home} />
+        <NewsSection news={data.news} />
+        <ReportsSection reports={data.reports} />
+      </div>
+      <FooterSection footer={footer} />
+      <SpeedInsights />
+    </>
+  );
 }
 
 export default Page;
