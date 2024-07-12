01a6e24d19415d8084a8f567a35cae046bc4bb96
Zerebel <86872654+Zerebel@users.noreply.github.com>
2024-07-12 14:56:41 +0100

    feat: Add GetInvestorsPage query to fetch investors page data
    This commit adds the GetInvestorsPage query to the queries.ts file, allowing us to fetch the data for the investors page. The query retrieves the first investors page item from the database, providing the necessary information for displaying the main image and overview of the investors page. This addition enhances the data fetching capabilities and enables the rendering of the investors page content.

--- utils/queries/queries.ts
+++ utils/queries/queries.ts
@@ -4,4 +4,18 @@ const QueryOmitDrafts = '!(_id in path("drafts.**"))';
 
 export const GetHomePage = groq`*[_type == "home"][0]`;
 
+export const GetInvestorsPage = groq`*[_type == "investors"][0]`;
+
 export const GetFooter = groq`*[_type == "footer"][0]`;
+
+export const GetPassword = groq`*[_type == 'investors' && defined(password)][0].password`;
+
+export const GetNewsFromRef = groq`*[_type == "news" && _id==$ref][0]`;
+
+export const GetNewsFromSlug = groq`*[_type == "news" && slug.current == $ref][0]`;
+
+export const GetReports = groq`*[_type == "reports"][0...6]`;
+
+export const GetReportBySlug = groq`*[_type == "reports" && slug.current == $ref][0]`;
+
+export const GetReportsCategories = groq`*[_type == "reportsCategory"]`;
