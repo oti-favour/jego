021da59b720eb8eac8083f4fe078a771355fcecf
Zerebel <86872654+Zerebel@users.noreply.github.com>
2024-07-17 08:48:47 +0100

    refactor: Update classNames in JegoPodSection for better readability and consistency

--- app/(main)/(sections)/jego_pod.tsx
+++ app/(main)/(sections)/jego_pod.tsx
@@ -1,7 +1,7 @@
 import CTA, { CTAPlain } from "@/components/CTA";
 import FeaturesStepper from "@/components/FeaturesStepper";
 import PageName from "@/components/PageName";
-import { HeadingFive, Paragraph } from "@/components/Typography";
+import { DynamicHeading, Paragraph } from "@/components/Typography";
 import WatchVideo from "@/components/WatchVideo";
 import JegoPodLayout from "@/components/home/JegoPodLayout";
 import { JegoPod } from "@/types/generated-types";
@@ -11,11 +11,15 @@ function JegoPodSection({ jegoPod }: { jegoPod: JegoPod }) {
 
   return (
     <JegoPodLayout>
-      <div className="grid h-full min-h-svh px-4 lg:container">
+      <div
+        className="grid h-full min-h-svh px-4 pb-12 lg:container"
+        id="jego-pods"
+      >
         <PageName
           text={overview.pageName}
           position="right"
           lineClassName="bg-white"
+          className="mb-24"
         />
         <div className="flex h-full items-center text-white">
           <div className="flex h-full min-h-full w-full flex-col justify-between gap-6 md:gap-12 lg:gap-16">
@@ -23,14 +27,18 @@ function JegoPodSection({ jegoPod }: { jegoPod: JegoPod }) {
               <WatchVideo videoUrl={video.videoUrl} title={video.title} />
             </div>
 
-            <div className="space-y-8">
+            <div className="mt-16 space-y-8">
               <div className="space-y-5">
                 <span className="font-gustavo text-lg font-bold uppercase md:text-[42px]">
                   introducing
                 </span>
-                <HeadingFive className="text-white" text={""}>
+                <DynamicHeading
+                  level="h6"
+                  className="normal-case text-white"
+                  text={""}
+                >
                   {overview.title}
-                </HeadingFive>
+                </DynamicHeading>
               </div>
 
               <div className="flex flex-col gap-4 md:flex-row md:gap-6">
@@ -43,11 +51,15 @@ function JegoPodSection({ jegoPod }: { jegoPod: JegoPod }) {
               </div>
             </div>
 
-            <div className="mb-6 grid grid-cols-1 grid-rows-2 md:mb-12 lg:grid-cols-2 lg:grid-rows-1">
+            <div className="mb-6 mt-12 grid grid-cols-1 grid-rows-2 md:mb-12 md:mt-0 lg:grid-cols-2 lg:grid-rows-1">
               <div className="flex lg:justify-center">
                 <Paragraph text={overview.description} />
               </div>
-              <FeaturesStepper features={pageFeatures} />
+              <div className="lg:flex lg:justify-end">
+                <div className="lg:w-10/12">
+                  <FeaturesStepper features={pageFeatures.slice(0, 2)} />
+                </div>
+              </div>
             </div>
           </div>
         </div>
