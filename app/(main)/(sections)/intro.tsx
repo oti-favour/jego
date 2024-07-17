417cb748baf58658b4d5d90b92a2d8838954a954
Zerebel <86872654+Zerebel@users.noreply.github.com>
2024-07-17 08:48:14 +0100

    refactor: Update classNames in IntroSection for better readability and consistency

--- app/(main)/(sections)/intro.tsx
+++ app/(main)/(sections)/intro.tsx
@@ -1,5 +1,5 @@
 import { CTAPlain } from "@/components/CTA";
-import { Caption, HeadingTwo, Paragraph } from "@/components/Typography";
+import { Caption, DynamicHeading, Paragraph } from "@/components/Typography";
 import IntroLayout from "@/components/home/IntroLayout";
 import { sanityImageUrl } from "@/lib/utils";
 import { Intro } from "@/types/generated-types";
@@ -14,13 +14,17 @@ function IntroSection({ intro }: { intro: Intro }) {
       <div className="flex h-full min-h-full items-center text-white lg:container">
         <div className="w-full space-y-16">
           <div className="flex max-w-5xl flex-col gap-6">
-            <HeadingTwo text={overview.title} className="max-w-4xl" />
+            <DynamicHeading
+              level="h3"
+              text={overview.title}
+              className="max-w-4xl"
+            />
             <Paragraph
               text={overview.description}
               className="block lg:hidden"
             />
             <CTAPlain
-              href=""
+              href="#about-us"
               text={overview.ctaText || ""}
               className="bg-brightTurquoise px-6 py-4"
             />
@@ -34,7 +38,7 @@ function IntroSection({ intro }: { intro: Intro }) {
             </div>
             <Paragraph
               text={overview.description}
-              className="-mt-4 hidden self-end lg:block"
+              className="mt-4 hidden lg:block"
             />
           </div>
         </div>
