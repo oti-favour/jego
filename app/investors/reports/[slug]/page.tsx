e2a2d7793e6e363199bf7ed091690300a952dd6e
Zerebel <86872654+Zerebel@users.noreply.github.com>
2024-07-17 08:47:26 +0100

    refactor: Update page.tsx to use PortableText for report content
    ```text
    Update page.tsx to use PortableText for rendering the report content. This change replaces the previous usage of the BlockContent component with PortableText, providing better flexibility and extensibility for rendering rich text content.

--- app/investors/reports/[slug]/page.tsx
+++ app/investors/reports/[slug]/page.tsx
@@ -1,10 +1,11 @@
-import BlockContent from "@/components/BlockContent";
+import { components } from "@/components/BlockContent";
 import FooterSection from "@/components/Footer";
 import GoBack from "@/components/GoBack";
 import Navbar from "@/components/Navbar";
 import { AuthorItem, NewsHeader } from "@/components/NewsItem";
 import { DynamicHeading } from "@/components/Typography";
 import { getInvestorsData, getReportFromSlug } from "@/hooks/getData";
+import { PortableText } from "next-sanity";
 import { notFound } from "next/navigation";
 
 async function NewsDetails({ params: { slug } }: { params: { slug: string } }) {
@@ -29,14 +30,14 @@ async function NewsDetails({ params: { slug } }: { params: { slug: string } }) {
             />
             <DynamicHeading
               level="h1"
-              className="!font-product text-2xl font-bold md:text-4xl lg:max-w-screen-md"
+              className="!font-product text-2xl font-bold capitalize md:text-4xl lg:max-w-screen-md"
             >
               {report.title}
             </DynamicHeading>
           </div>
           <AuthorItem {...report.author} />
-          <div className="whitespace-pre-line lg:max-w-screen-md">
-            <BlockContent value={report.content} />
+          <div className="whitespace-pre-line font-light leading-[30px] text-[#878787] lg:max-w-screen-md">
+            <PortableText value={report.content} components={components} />
           </div>
         </div>
       </div>
