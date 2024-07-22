150ca0bb33f3e3623261add42898f15c8baa0ea7
Zerebel <86872654+Zerebel@users.noreply.github.com>
2024-07-22 19:24:34 +0100

    refactor: Optimize image loading and update classNames in BlockContent.tsx

--- components/BlockContent.tsx
+++ components/BlockContent.tsx
@@ -1,45 +1,107 @@
 import { sanityImageUrl } from "@/lib/utils";
-import { PortableText } from "@portabletext/react";
-import { getImageDimensions } from "@sanity/asset-utils";
+import { PortableText, PortableTextReactComponents } from "@portabletext/react";
+import Image from "next/image";
+import Link from "next/link";
 
 function BlockContent({ value }: { value: any }) {
-  return <PortableText value={value} components={components} />;
+  return <PortableText value={value} components={RichTextComponent} />;
 }
 
 export default BlockContent;
 
-// Barebones lazy-loaded image component
-const SampleImageComponent = ({
-  value,
-  isInline,
-}: {
-  value: any;
-  isInline: any;
-}) => {
-  const { width, height } = getImageDimensions(value);
-  return (
-    // eslint-disable-next-line @next/next/no-img-element
-    <img
-      src={sanityImageUrl(value)}
-      alt={value.alt || " "}
-      loading="lazy"
-      style={{
-        // Display alongside text if image appears inside a block text span
-        display: isInline ? "inline-block" : "block",
-
-        // Avoid jumping around with aspect-ratio CSS property
-        aspectRatio: width / height,
-        width: "100%",
-      }}
-    />
-  );
-};
-
-export const components = {
+export const RichTextComponent: Partial<PortableTextReactComponents> = {
   types: {
-    image: SampleImageComponent,
+    image: ({ value }: any) => {
+      return (
+        <div className="relative mx-auto my-5 h-96 w-full">
+          <Image
+            className="object-contain"
+            src={sanityImageUrl(value)}
+            fill
+            alt="..."
+          />
+        </div>
+      );
+    },
+  },
+  list: {
+    bullet: ({ children }: any) => {
+      return (
+        <ul className="ml-10 list-disc space-y-2 py-5 sm:space-y-3">
+          {children}
+        </ul>
+      );
+    },
+    number: ({ children }: any) => {
+      return (
+        <ul className="ml-10 list-decimal space-y-2 py-5 sm:space-y-3">
+          {children}
+        </ul>
+      );
+    },
+  },
+  block: {
+    normal: ({ children }: any) => (
+      <p className="my-2 text-base md:my-5">{children}</p>
+    ),
+    h1: ({ children }: any) => (
+      <h1 className="my-3 text-2xl font-semibold leading-tight sm:text-3xl">
+        {children}
+      </h1>
+    ),
+    h2: ({ children }: any) => (
+      <h2 className="my-3 text-2xl font-semibold leading-tight md:my-5">
+        {children}
+      </h2>
+    ),
+    h3: ({ children }: any) => (
+      <h3 className="my-3 text-xl font-semibold leading-tight md:my-5">
+        {children}
+      </h3>
+    ),
+    h4: ({ children }: any) => (
+      <h4 className="my-3 text-xl font-semibold leading-tight md:my-5">
+        {children}
+      </h4>
+    ),
+    h5: ({ children }: any) => (
+      <h5 className="my-3 text-lg font-semibold leading-tight md:my-5">
+        {children}
+      </h5>
+    ),
+    h6: ({ children }: any) => (
+      <h6 className="text-md my-3 font-semibold leading-tight md:my-5">
+        {children}
+      </h6>
+    ),
+    blockquote: ({ children }: any) => (
+      <blockquote className="mt-4 border-l-4 border-gray-400 pl-4 italic text-gray-600 sm:mt-6 md:mt-8">
+        {children}
+      </blockquote>
+    ),
 
-    // Any other custom types you have in your content
-    // Examples: mapLocation, contactForm, code, featuredProjects, latestNews, etc.
+    customHeading: ({ children }: any) => (
+      <h2 className="text-lg text-primary text-purple-700">{children}</h2>
+    ),
+  },
+  marks: {
+    link: ({ children, value }: any) => {
+      const rel = !value.href.startsWith("/")
+        ? "noreferrer noopener"
+        : undefined;
+      return (
+        <Link
+          href={value.href}
+          rel={rel}
+          target="_blank"
+          className="text-blue-700 hover:text-blue-900"
+        >
+          {children}
+        </Link>
+      );
+    },
+  },
+  hardBreak: ({ children }: any) => {
+    return <span className="space-y-1">{children}</span>;
   },
 };
