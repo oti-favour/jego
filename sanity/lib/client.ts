a97b4c83f11d8cb710ae29b28cd2b8d2737f2e01
Zerebel <86872654+Zerebel@users.noreply.github.com>
2024-08-06 08:31:43 +0100

    refactor: Update `sanity/lib/client.ts` to include token in createClient options

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,11 +1,12 @@
-import { createClient } from 'next-sanity'
+import { createClient } from "next-sanity";
 
-import { apiVersion, dataset, projectId, useCdn } from '../env'
+import { apiVersion, dataset, projectId, token, useCdn } from "../env";
 
 export const client = createClient({
   projectId,
   dataset,
   apiVersion,
   useCdn,
-  perspective: 'published',
-})
+  token,
+  perspective: "published",
+});
