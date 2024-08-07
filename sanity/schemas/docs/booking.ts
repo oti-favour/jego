164150e08f809854e5b3082307c2e2c527a72b44
Zerebel <86872654+Zerebel@users.noreply.github.com>
2024-08-07 14:58:19 +0100

    feat: Add preview field to Booking Pods schema
    This commit adds a preview field to the Booking Pods schema in the booking.ts file. The preview field is used to customize the display of the booking pods in the Sanity Studio. This enhancement improves the user experience by providing a more informative and visually appealing representation of the booking pods.

--- sanity/schemas/docs/booking.ts
+++ sanity/schemas/docs/booking.ts
@@ -5,10 +5,41 @@ export default defineType({
   type: "document",
   title: "Booking Pods",
   description: "The booking pods of the site",
+  preview: {
+    select: {
+      title: "calendlyURL",
+    },
+    prepare(selection) {
+      return {
+        title: "Booking Pod",
+      };
+    },
+  },
   fields: [
-    defineField({
-      name: "title",
-      type: "string",
-    }),
+    defineField(
+      {
+        name: "calendlyURL",
+        type: "url",
+        validation: (rule) => rule.required(),
+        codegen: { required: true },
+        description: "The URL to the calendly event",
+      },
+      {
+        strict: false,
+      },
+    ),
+
+    defineField(
+      {
+        name: "maptilerKey",
+        type: "string",
+        validation: (rule) => rule.required(),
+        codegen: { required: true },
+        description: "The MapTiler API key",
+      },
+      {
+        strict: false,
+      },
+    ),
   ],
 });
