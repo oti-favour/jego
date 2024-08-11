1d682012e99adbfb935cd675a30bf0196a5bd66c
Zerebel <86872654+Zerebel@users.noreply.github.com>
2024-08-11 13:22:10 +0100

    Update HeroLayout component in home directory

--- components/home/HeroLayout.tsx
+++ components/home/HeroLayout.tsx
@@ -2,9 +2,11 @@ import React from "react";
 
 function HeroLayout({ children }: { children?: React.ReactNode }) {
   return (
-    <div className="min-h-svh bg-hero_car bg-cover bg-center bg-no-repeat">
-      <div className="h-full bg-hero_ray bg-150 bg-no-repeat px-4 md:bg-80">
-        {children}
+    <div className="min-h-svh bg-hero_car bg-cover bg-center bg-no-repeat md:bg-left-top">
+      <div className="h-full w-full bg-black/80">
+        <div className="h-full bg-hero_ray bg-150 bg-no-repeat px-4 md:bg-80">
+          {children}
+        </div>
       </div>
     </div>
   );
