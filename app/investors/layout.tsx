4e8668907ba709c166955cd886fc45aa97611ba6
Zerebel <86872654+Zerebel@users.noreply.github.com>
2024-08-12 07:49:30 +0100

    Update layout.tsx in investors app directory

--- app/investors/layout.tsx
+++ app/investors/layout.tsx
@@ -4,6 +4,38 @@ import "../globals.css";
 export const metadata: Metadata = {
   title: "Jégo",
   description: "Welcome to Jégo",
+  openGraph: {
+    title: "Jégo",
+    // keep description above 50 characters
+    description:
+      "Welcome to Jégo Investment Page, Get the latest financial reports and updates on Jégo's innovative modular energy systems and electric vehicles.",
+    type: "website",
+    url: "https://jegopods.com",
+    images: [
+      {
+        url: "https://jegopods.com/assets/logo.svg",
+        width: 1200,
+        height: 630,
+        alt: "Jégo",
+      },
+    ],
+  },
+  applicationName: "Jégo",
+  twitter: {
+    card: "summary_large_image",
+    site: "@jegopods",
+    title: "Jégo",
+    description:
+      "Welcome to Jégo Investment Page, Get the latest financial reports and updates on Jégo's innovative modular energy systems and electric vehicles.",
+    images: [
+      {
+        url: "https://jegopods.com/assets/logo.svg",
+        width: 1200,
+        height: 630,
+        alt: "Jégo",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
