8d80631ec17c098240221ca44d53b73a951066fd
Zerebel <86872654+Zerebel@users.noreply.github.com>
2024-08-14 08:31:02 +0100

    feat: Refactor HeroSection component and import HomeOverview for improved hero section on the home page

--- app/(main)/(sections)/hero.tsx
+++ app/(main)/(sections)/hero.tsx
@@ -1,10 +1,9 @@
 import { FadeInUp } from "@/components/Animation";
-import CTA, { CTASecondary } from "@/components/CTA";
 import FeaturesStepper from "@/components/FeaturesStepper";
-import HeadingOne, { Paragraph } from "@/components/Typography";
 import HeroLayout from "@/components/home/HeroLayout";
-import ScrollIcon from "@/public/assets/scroll-icon.svg";
-import { Hero, Overview } from "@/types/generated-types";
+import HeroOverview from "@/components/home/HomeOverview";
+
+import { Hero } from "@/types/generated-types";
 
 function HeroSection({ hero }: { hero: Hero }) {
   return (
@@ -25,29 +24,6 @@ function HeroSection({ hero }: { hero: Hero }) {
         </div>
       </div>
     </HeroLayout>
-  );
-}
-
-function HeroOverview({ overview }: { overview: Overview }) {
-  return (
-    <div className="mt-16 space-y-16 text-white">
-      <div className="flex flex-col items-end justify-end">
-        <div className="max-w-fit">
-          <Paragraph text={overview.description} />
-          {/* <hr className="mt-6 border-[#7E7E7E96]" /> */}
-        </div>
-      </div>
-      <div className="max-w-5xl space-y-8">
-        <HeadingOne text={overview.title} />
-        <div className="flex justify-between">
-          <div className="grid max-w-fit flex-1 grid-rows-2 gap-4 lg:grid-cols-2 lg:grid-rows-1">
-            <CTA href="" text={overview.ctaText || ""} />
-            <CTASecondary text="Learn More" />
-          </div>
-          <ScrollIcon className="animate-bounce self-end" />
-        </div>
-      </div>
-    </div>
   );
 }
 
